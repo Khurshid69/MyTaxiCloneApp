8ec811f5d30745e6d4045dc0690ded94309ea031
khurshid <khurshidrakhimov06@gmail.com>
2021-12-16 14:15:55 +0500

    Initial Commit

---
+++ MyTaxiClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTaxiClone
+//
+//  Created by user on 16/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
