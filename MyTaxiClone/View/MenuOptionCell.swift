3e7c51aeeaa44fd419d782ad42a1b4f4485e9af6
khurshid <khurshidrakhimov06@gmail.com>
2021-12-17 20:02:20 +0500

    someChanges

--- MyTaxiClone/View/MenuOptionCell.swift
+++ MyTaxiClone/View/MenuOptionCell.swift
@@ -45,9 +45,6 @@ class MenuOptionCell: UITableViewCell {
     }
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        fatalError("init(coder: ) has not been implemented")
     }
-    
-    // MARK: - Handlers
-    
 }
